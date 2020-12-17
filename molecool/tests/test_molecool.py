9b8dd7d25926e71f3c104b647749e1c85fdf6a88
Ernesto Cortes <ernie2k@gmail.com>
2020-12-17 13:37:53 -0500

    New testing.

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -6,6 +6,7 @@ Unit and regression test for the molecool package.
 import molecool
 import pytest
 import sys
+import numpy as np
 
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
@@ -21,3 +22,14 @@ def test_molecular_mass():
 
     assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
 
+def test_calculate_angle():
+
+    theta1 = np.array([0, 0, -1]) 
+    theta2 = np.array([0, 0, 0])
+    theta3 = np.array([1, 0, 0]) 
+
+    expected_angle = 90
+
+    calculated_angle = molecool.calculate_angle(theta1, theta2, theta3, degrees=True)
+
+    assert expected_angle == calculated_angle
